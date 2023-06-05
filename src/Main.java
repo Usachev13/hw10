31410d37c6b01782e3f0d6b66330a36ae00168a0
Usachev13 <2893318@gmail.com>
2023-06-05 17:56:19 +0300

    task1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("HW 10 start!");
+        task1();
+    }
+
+    /* task1 */
+    public static void task1() {
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
+
     }
 }
