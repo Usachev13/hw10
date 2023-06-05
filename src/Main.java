8e7c12ec9cf1c0179b7aa8389bea674b0e5af59c
Usachev13 <2893318@gmail.com>
2023-06-05 17:59:50 +0300

    task2 done

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,28 @@ public class Main {
 
         System.out.println("HW 10 start!");
         task1();
+        task2();
     }
 
     /* task1 */
     public static void task1() {
+        System.out.println("Task 1");
         String firstName = "Ivan ";
         String middleName = "Ivanovich";
         String lastName = "Ivanov ";
         String fullName = lastName + firstName + middleName;
         System.out.println(fullName);
 
+    }
+
+    /* task2 */
+
+    public static void task2() {
+        System.out.println("Task 2");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName. toUpperCase());
     }
 }
